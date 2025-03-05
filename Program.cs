538ed3cde5bd804ddb0fc7ee85e73517932d25d5
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-04 21:25:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int maiorValor = int.MinValue; 
+        int posicao = 0; 
+
+        for (int i = 1; i <= 100; i++) 
+        {
+            int valor = int.Parse(Console.ReadLine(); 
+
+            if (valor > maiorValor)
+            {
+                maiorValor = valor;
+                posicao = i; 
+            }
+        }
+
+        
+        Console.WriteLine(maiorValor);
+        Console.WriteLine(posicao);
+    }
+}
+
+
